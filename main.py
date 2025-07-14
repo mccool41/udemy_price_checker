c2a8545c650e0bb40a67c81b81049f7f4153f1da
Chris <43861839+mccool41@users.noreply.github.com>
2025-07-14 20:09:23 +0100

    Prints price and discounted price of the course

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import requests
 
 print("Checking course page at https://www.udemy.com/course/automate/")
-response = requests.get("https://www.udemy.com/course/automate/")
-print("Status code:", response.status_code)
+response = requests.get("https://www.udemy.com/api-2.0/courses/automate/?fields[course]=price,discount_price,is_paid")
+
+if response.status_code == 200:
+    data = response.json()
+    print("Price:", data["price"])
+    print("Discounted price:", data["discount_price"])
+else:
+    print("Failed to fetch course info:", response.status_code)
