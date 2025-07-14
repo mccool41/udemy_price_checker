e2d0a86c2ecbc8ecf97875b1dc7a0c84cdefefce
Chris <43861839+mccool41@users.noreply.github.com>
2025-07-14 20:32:46 +0100

    Also shows percentage of the discount if there is one

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ response = requests.get("https://www.udemy.com/api-2.0/courses/automate/?fields[
 
 if response.status_code == 200:
     data = response.json()
-    print("Price:", data["price"])
-    print("Discounted price:", data["discount_price"])
+
+    if data["discount_price"] is not None:
+        discountPercentage = round((data["price"] - data["discount_price"]) / data["price"] * 100, 2)
+        print("Original price: ", data["price"])
+        print("Discounted price: ", data["discount_price"])
+        print("Discount percentage: ", discountPercentage, "%")
+    else:
+        print("Price: ", data["price"])
+        print("No discount available")
+
 else:
     print("Failed to fetch course info:", response.status_code)
