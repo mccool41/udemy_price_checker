94afb3474b81d3c383dfa98c37ec9f1a759adb35
Chris <43861839+mccool41@users.noreply.github.com>
2025-07-14 21:40:00 +0100

    Now using powershell for the notification so it stays in the Windows action centre

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import requests
-from plyer import notification
+import subprocess
+
+def runNotification(title, message):
+    powershell_command = f'''
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+    $template = [Windows.UI.Notifications.ToastTemplateType]::ToastText02
+    $xml = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent($template)
+    $text = $xml.GetElementsByTagName("text")
+    $text.Item(0).AppendChild($xml.CreateTextNode("{title}")) > $null
+    $text.Item(1).AppendChild($xml.CreateTextNode("{message}")) > $null
+    $toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
+    $appId = "Udemy Price Checker"
+    $notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($appId)
+    $notifier.Show($toast)
+    '''
+    subprocess.run(["powershell", "-Command", powershell_command])
 
 # Checking course page at https://www.udemy.com/course/automate/
 response = requests.get("https://www.udemy.com/api-2.0/courses/automate/?fields[course]=price,discount_price,is_paid")
@@ -9,23 +24,17 @@ if response.status_code == 200:
 
     if data["discount_price"] is not None:
         discountPercentage = round((data["price"] - data["discount_price"]) / data["price"] * 100, 2)
-        notification.notify(
-            title = "Udemy Python Automation Discount Alert",
-            message = "Discounted to " + data["discount_price"] + " that's " + discountPercentage + "% off!",
-            timeout = 10
-        )
+        title = "Udemy Python Automation on Sale Now!"
+        message = "The course is " + data["price"] + " that's " + discountPercentage + "% off!"
+
     else:
-        notification.notify(
-            title = "Udemy Python Automation Discount Alert",
-            message = "No discount available :( the price is: " + data["price"],
-            timeout = 10
-        )
+        title = "Udemy Python Automation isn't on Sale :("
+        message = "The course is " + data["price"] + " with no discount available."
 
 else:
     # Printing the error code to console if needed
     print("Failed to fetch course info:", response.status_code)
-    notification.notify(
-            title = "Udemy Python Automation Discount Alert",
-            message = "Failed to fetch course info:" + response.status_code,
-            timeout = 10
-        )
+    title = "Udemy Python Automation is Having an Issue :'("
+    message = "Failed to fetch course info: " + response.status_code
+
+runNotification(title, message)
