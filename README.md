c35eedaec15e1e36040a531a989b138868a38c60
Chris <43861839+mccool41@users.noreply.github.com>
2025-07-16 20:36:28 +0100

    Added the ability to check multiple Udemy links

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
 # Udemy Price Checker 🛒
 
-A Python script that checks if [this Udemy course](https://www.udemy.com/course/automate/) is on sale and sends a Windows notification when it is.
+A Python script that checks if any Udemy courses you want are on sale and sends a Windows notification when it is.
 
 ---
 
 ## 💡 What it does
 
-- Checks Udemy every 15 minutes to see if the course is on sale
+- Checks Udemy every 15 minutes to see if any of the courses are on sale
 - Sends a Windows notification if:
   - The course is on sale
   - There’s a problem fetching the course
 - The notification includes the discounted price, percentage off, and stays visible in your Windows Action Center until dismissed
-- Stops running after a notification comes through so you don't get spammed
+- Stops running for a course after a notification comes through so you don't get spammed but continues checking on any remaining courses (not on sale or not producing a URL error)
 
 ---
 
-## ⚙️ How to Use
+## ⚙️ How to Install and Use
+
+### Install
 
 1. Clone this repo:
    ```bash
@@ -35,6 +37,18 @@ A Python script that checks if [this Udemy course](https://www.udemy.com/course/
         - New → Action: "Start a program"
         - Program/script: browse to where you saved "launch_silent.vbs"
     - Save the task.
+    - Logging out and back in will be required unless you run the task manually the first time, you can do this by:
+        - You can run this manually the first time by clicking on Task Scheduler Library on the left hand menu
+        - Find and right click on your new task and select Run
+
+### Use
+
+1. Edit the udemy_links.txt to include all the Udemy course links you want to keep track of, one link per line
+<p align="center">
+  <img src="images/TextFileExample.png" alt="Screenshot of what the text file would look like with some links inside" />
+</p>
+
+2. That should be it, if you do want to run it manually without adding to the Windows Task Scheduler (skipping option 3 of the install above) then you can and everything should work until you close the process, sign out or it finds a sale (or URL error) for each course you added for it to check 🤞
 
 ---
 
@@ -43,11 +57,15 @@ A Python script that checks if [this Udemy course](https://www.udemy.com/course/
 
 Notifications use PowerShell to call Windows native toast system.
 
+This does need all files in the same folder when used to function correctly
+
 ---
 
 ## ✅ Example Notification
-Title:
-Udemy Python Automation on Sale Now!
+**Title:**
+Udemy Automate Course on Sale Now!
 
-Message:
+**Message:**
 The course is £20 — that's 78.95% off!
+
+![Screenshot of what the alerts look like in the action centre](images/Demo.png)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 import subprocess
 import time
+import os
 
-def runNotification(title, message):
+def run_notification(title, message):
     powershell_command = f'''
     [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
     $template = [Windows.UI.Notifications.ToastTemplateType]::ToastText02
@@ -17,33 +18,51 @@ def runNotification(title, message):
     '''
     subprocess.run(["powershell", "-Command", powershell_command])
 
-def check_udemy_price():
-    # Checking course page at https://www.udemy.com/course/automate/
-    response = requests.get("https://www.udemy.com/api-2.0/courses/automate/?fields[course]=price,discount_price,is_paid")
+def check_udemy_price(udemy_course_name):
+    response = requests.get(f"https://www.udemy.com/api-2.0/courses/{udemy_course_name}/?fields[course]=price,discount_price,is_paid")
+    udemy_course_title = udemy_course_name.replace("-", " ").title()  # Format course name for notification
 
     if response.status_code == 200:
         data = response.json()
-
         if data["discount_price"] is not None:
-            originalPrice = float(data["price"].replace("£", ""))
-            discountPrice = float(data["discount_price"]["amount"])
-            discountPercentage = round((originalPrice - discountPrice) / originalPrice * 100, 2)
-            discountPriceStr = data["discount_price"]["price_string"]
-            title = "Udemy Python Automation on Sale Now!"
-            message = "The course is " + discountPriceStr + " that's " + str(discountPercentage) + "% off!"
-            runNotification(title, message)
+            original_price = float(data["price"].replace("£", ""))
+            discount_price = float(data["discount_price"]["amount"])
+            discount_percentage = round((original_price - discount_price) / original_price * 100, 2)
+            discount_price_str = data["discount_price"]["price_string"]
+            title = f"Udemy {udemy_course_title} Course on Sale Now!"
+            message = f"The course is {discount_price_str} that's {str(discount_percentage)} % off!"
+            run_notification(title, message)
             return True
 
         else:
             return False # not on sale, continue checking
 
     else:
-        title = "Udemy Link is Having an Issue"
-        message = "Failed to fetch course info: " + response.status_code
-        runNotification(title, message)
+        title = f"Udemy {udemy_course_title} Course is Having an Issue"
+        message = f"Failed to fetch course info: {str(response.status_code)}"
+        run_notification(title, message)
         return True
 
+def get_udemy_urls_from_file():
+    udemy_course_name_array = []
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, "udemy_links.txt")
+    with open(file_path, "r") as file:
+        course_links = [line.strip() for line in file if line.strip()]
+    
+    for link in course_links:
+        udemy_course_name_array.append(link.split("/course/")[1].split("/")[0].split("?")[0])
+    
+    return udemy_course_name_array
+
 while True:
-    if check_udemy_price():
-        break  # exit early if on sale or URL issue
+    udemy_course_name_array = get_udemy_urls_from_file()
+    for udemy_course_name in udemy_course_name_array[:]: # create a shallow copy to iterate over
+        udemy_course_on_sale = check_udemy_price(udemy_course_name)
+        if udemy_course_on_sale:
+            udemy_course_name_array.remove(udemy_course_name)
+
+    if len(udemy_course_name_array) == 0:
+        break
+    
     time.sleep(900)  # 15 minutes
